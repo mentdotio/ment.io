1224072ad6dce0441ab1ac10ca65f27ceffc745e
newswim <danminshew@gmail.com>
2017-09-09 18:10:42 -0500

    Add an annoying 'coming soon' section

--- app/src/elements/Input.js
+++ app/src/elements/Input.js
@@ -1,15 +1,10 @@
-// An controled React component
-
-
-import React,{Component} from 'react';
+import React,{ Component } from 'react'
 
 export default class Input extends Component {
 
     render() {
         return (
-            <div className="class-name">
-                content
-            </div>
-        );
+            <div>Some content</div>
+        )
     }
 }
--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,14 +1,21 @@
 // Home page
 
-import React,{Component} from 'react';
+import React,{ Component } from 'react'
+import './Home.css'
 
 export default class Home extends Component {
 
     render() {
         return (
-            <div className="home-component">
-                Home
-            </div>
-        );
+            <div id="container">
+              <div className="neon">Coming</div>
+              <div className="flux">Soon</div>
+
+            {/*
+              this placeholder is based on the work of Kelly Kovalcik, just slightly modified
+              See the original here: http://codepen.io/kellykov/pen/NArGoE
+            */}
+          </div>
+        )
     }
 }
